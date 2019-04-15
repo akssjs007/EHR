8b7daa8e6d70892cfe32ed0309d9e15c15ae176a
Basil Singh <basil@iitk.ac.in>
2019-04-16 02:10:55 +0530

    Update flogic.js

--- flogic.js
+++ flogic.js
@@ -43,3 +43,63 @@ async function gettingAppointment(catx) {
   const appointRegistry = await getAssetRegistry('org.example.mynetwork.Appointment');
   await appointRegistry.update(catx.appoint);
 }
+
+/**
+ * Sample transaction processor function.
+ * @param {org.ehr.basic.consult} ctx The sample transaction instance.
+ * @transaction
+ */
+async function gettingChecked(ctx) {
+  if(!(ctx.presc.isMedPrescribed || ctx.presc.isTestPrescribed )){
+    throw new Error('Prescription should prescibe either test or medicine')
+  }
+  if (ctx.appoint.status === 'PENDING'){
+    throw new Error('Appointment yet to be confirmed!')
+  }
+  if (ctx.appoint.appointmentId != ctx.presc.appoint.appointmentId){
+    throw new Error('Prescription not matching with the appointment!')
+  }
+  if (ctx.appoint.status === 'CONSULTED'){
+    throw new Error('Appointment already consulted!')
+  }
+  if (ctx.appoint.status === 'REJECTED'){
+    throw new Error('Appointment already rejected!')
+  }
+  if(ctx.patient.id != ctx.appoint.patient.id){
+    throw new Error('Patient Proxy not allowed!')
+  }
+  if ( ctx.presc.appoint.doctor.id != ctx.doctor.id){
+    throw new Error('You are not the designated doctor')
+  }
+
+  ctx.appoint.status = 'CONSULTED';
+  ctx.appoint.group = ctx.patient.disease;
+  ctx.appoint.consultanceFee = ctx.doctor.consultanceFee;
+  ctx.patient.debt = ctx.patient.debt + ctx.appoint.consultanceFee;
+  if (ctx.appoint.isInsured || (ctx.appoint.insuranceId.ensuredAmount >= ctx.appoint.consultanceFee)){
+    ctx.appoint.insuranceId.ensuredAmount = ctx.appoint.insuranceId.ensuredAmount - ctx.appoint.consultanceFee;
+  } else {
+    let test = ctx.appoint.consultanceFee - ctx.appoint.insuranceId.ensuredAmount;
+    ctx.patient.debt = ctx.patient.debt + test;
+    ctx.appoint.insuranceId.ensuredAmount = 0
+    ctx.appoint.isInsured = FALSE;
+  }
+  
+  const pre = ctx.presc;
+  
+  if (pre.prescribed) {
+      pre.prescribed.push(ctx.medicine);
+  } else {
+      pre.prescribed = [ctx.medicine];
+  }
+  
+  pre.test = 'BLOODTEST';
+  
+  
+  const appointRegistry = await getAssetRegistry('org.example.mynetwork.Appointment');
+  await appointRegistry.update(ctx.appoint);
+  const patientRegistry = await getParticipantRegistry('org.example.mynetwork.Patient');
+  await patientRegistry.update(ctx.patient);
+  const prescriptionRegistry = await getAssetRegistry('org.example.mynetwork.Prescription');
+  await prescriptionRegistry.update(pre);
+}
